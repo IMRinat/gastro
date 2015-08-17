6ed5a52a7ef0e5463287bfb105e4a8f1fa6349a1
Rinat <shamrinat@mail.ru>
2015-08-17 14:54:51 +0500

    replace & for %26

--- soaplist.js
+++ soaplist.js
@@ -49,19 +49,19 @@ M.server = M.proxy+"https://45.r-mis.ru/";
 M.loginURL = M.server+'cas/login?service=https%3A%2F%2F45.r-mis.ru%2Fj_spring_cas_security_check';
 
 M.zogurl =M.server+'cases/records/ajax?'+
-    'page=1&size=130&organizationId=25&departmentTypeId=3'+
-    '&filterDepartmentId=481&createDate.from=31.10.2014&createDate.to=30.12.2015'+
-    '&fields=id&fields=patientShortName&fields=uid&fields=admissionCaseDate&fields=admissionDate'+
-    '&fields=admissionTime&fields=outcomeDate&fields=outcomeTime&fields=bedDaysAmount&fields=doctor'+
-    '&fields=departmentName&fields=diagnosisCode&fields=mainDiagnosisNote&fields=profileName'+
-    '&fields=resultName&fields=outcomeName&fields=signDate&fields=ward&fields=healthRecordExtension'+
-    '&fields=healthRecordRoot&fields=patientId&fields=patientLabel&fields=healthRecordSigned&fields=canBeRemoved';
+    'page=1%26size=130%26organizationId=25%26departmentTypeId=3'+
+    '%26filterDepartmentId=481%26createDate.from=31.10.2014%26createDate.to=30.12.2015'+
+    '%26fields=id%26fields=patientShortName%26fields=uid%26fields=admissionCaseDate%26fields=admissionDate'+
+    '%26fields=admissionTime%26fields=outcomeDate%26fields=outcomeTime%26fields=bedDaysAmount%26fields=doctor'+
+    '%26fields=departmentName%26fields=diagnosisCode%26fields=mainDiagnosisNote%26fields=profileName'+
+    '%26fields=resultName%26fields=outcomeName%26fields=signDate%26fields=ward%26fields=healthRecordExtension'+
+    '%26fields=healthRecordRoot%26fields=patientId%26fields=patientLabel%26fields=healthRecordSigned%26fields=canBeRemoved';
 
 
-M.uslurl = M.server+'cases/finder?code=medicalCard.service.for.organization&params'+
+M.uslurl = M.server+'cases/finder?code=medicalCard.service.for.organization%26params'+
     '={%22entityId%22%3Anull%2C%22orgId%22%3A25%2C%22resGroupId%22%3Anull%2C%22date%22%3A%22'+
-    '14.11.2014%22%2C%22root%22%3Anull}&text={0}&page=1';
+    '14.11.2014%22%2C%22root%22%3Anull}%26text={0}%26page=1';
 
-M.uslpaturl = M.server+'cases/tree/services/jsonp?value={0}&page=1&start=0&limit=25&callback=';
+M.uslpaturl = M.server+'cases/tree/services/jsonp?value={0}%26page=1%26start=0%26limit=25%26callback=';
 
 M.back1 = '#eyJzdGF0ZSI6eyJUYWJCb2R5Ijp7ImFjdGl2ZSI6ZmFsc2V9LCJwcm90b2NvbFRhYlRhYkJvZHkiOnsiYWN0aXZlIjp0cnVlfX19';
